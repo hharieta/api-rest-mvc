ad246edcd9681b1353ca47259c1c060bd976db05
hharieta <L20530228@cancun.tecnm.mx>
2022-12-05 17:41:21 -0500

    created templates and styles sheets

--- app.py
+++ app.py
@@ -16,5 +16,26 @@ def home():
     return render_template("home.html", people=people)
 
 
+# Create a URL route in our application for "/people"
+@app.route("/people")
+@app.route("/people/<int:person_id>")
+def people(person_id=""):
+    """
+    This function just responds to the browser URL
+    localhost:5000/people
+
+    :return:        the rendered template "people.html"
+    """
+    return render_template("people.html", person_id=person_id)
+
+
+# Create a URL route to the notes page
+@app.route("/people/<int:person_id>")
+@app.route("/people/<int:person_id>/notes")
+@app.route("/people/<int:person_id>/notes/<int:note_id>")
+def notes(person_id, note_id=""):
+    return render_template("notes.html", person_id=person_id, note_id=note_id)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
